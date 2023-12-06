e1adf86fe8461581825981abab60bb2f415d0c38
curtis-jin <92815013+curtis-jin@users.noreply.github.com>
2023-12-06 15:28:08 -0800

    Update dataProcessing.c

--- dataProcessing.c
+++ dataProcessing.c
@@ -1,25 +1,5 @@
 #include "dataProcessing.h"
 
-void* readPhotoresistor(void* arg) {
-    while (1) {
-        // Read A2D
-        int a2dReading = getVoltageReading(1);
-
-        // Get current timestamp
-        long long timestamp = getTimeinUs();
-
-        // Store the reading and timestamp in the buffers
-        pthread_mutex_lock(&bufferMutex);
-        buffer[bufferIndex++] = a2dReading;
-        buffer[bufferIndex++] = timestamp;
-        pthread_mutex_unlock(&bufferMutex);
-
-        // Sleep for 1ms
-        sleepForMs(1);
-    }
-
-    return NULL;
-}
 // Function to extract and process samples by the analysis module
 int extractAndProcessSamples() {
     
