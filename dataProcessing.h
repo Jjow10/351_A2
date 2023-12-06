789aa86f8776723000a93e84a6979c3f861f168e
curtis-jin <92815013+curtis-jin@users.noreply.github.com>
2023-12-06 15:12:22 -0800

    Update dataProcessing.h

--- dataProcessing.h
+++ dataProcessing.h
@@ -11,6 +11,15 @@
 #include "displayLED.h"
 #include "joystickState.h"
 
+#define BUFFER_SIZE 2000 // Adjust the size based on your needs
+
+int buffer[BUFFER_SIZE];
+int bufferIndex  = 0;
+pthread_mutex_t bufferMutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Function to read the photoresistor and store the value in the buffer
+void* readPhotoresistor(void* arg);
+
 //Function to extract and process samples by the analysis module
 int extractAndProcessSamples(void); 
 
